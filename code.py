5d16e9287004abd6dcc2d3bc2a6a4c5660334218
JHJ20 <jin010630@dgu.ac.kr>
2024-09-26 15:57:26 +0900

    개인과제6-ossprac2

--- code.py
+++ code.py
@@ -1,8 +1,10 @@
 Name = input("이름을 입력하세요. : ")
 number = input("학번을 입력하세요. : ")
 department = input("학과를 입력하세요. : ")
+grade = input("학년을 입력하세요. : ")
 
 print("\n\n<출력>")
 print("이름 : " ,Name)
 print("학번 : " ,number)
 print("학과 : " ,department)
+print("학년 : " ,grade)
